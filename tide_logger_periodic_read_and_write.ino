6e5114f483e353980bf5fc047d9675f975418ea4
Colin Murtaugh <cmurtaugh@gmail.com>
2021-11-06 14:27:04 -0400

    Update and rename tide_logger.ino to tide_logger_periodic_read_and_write.ino

--- tide_logger_periodic_read_and_write.ino
+++ tide_logger_periodic_read_and_write.ino
@@ -1,10 +1,7 @@
-
 #include "SD.h"
 #include "RTClib.h"
 
 RTC_PCF8523 rtc;
-
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 // Example code for DYP-ME007YS sensor
 
@@ -22,53 +19,38 @@ SoftwareSerial mySerial(2, -1);
 
 #endif
 
-// the digital pins that connect to the LEDs
-#define redLEDpin 3
-#define greenLEDpin 4
-
-#define IO_USERNAME  "cmurtaugh"
-#define IO_KEY       "aio_XvXc79nKkznCs8bkC423b9AAAnZm"
+#define PRINT_TO_SERIAL 1
 
 #define CONTROL_PIN 5   // This is the YELLOW wire, can be any data line
+#define CHIP_SELECT 10 
+#define SYNC_INTERVAL 10000  // mills between calls to flush() - to write data to the card
+#define LOG_INTERVAL 5000
+
 
 int16_t distance;  // The last measured distance
 bool newData = false; // Whether new data is available from the sensor
 uint8_t buffer[4];  // our buffer for storing data
 uint8_t idx = 0;  // our idx into the storage buffer
-File logfile;
-const int chipSelect = 10;
-
-#define SYNC_INTERVAL 10000  // mills between calls to flush() - to write data to the card
-#define LOG_INTERVAL 5000
-//const int SYNC_INTERVAL = 1000 * 60 * 5; // mills between calls to flush() - to write data to the card
-//const int LOG_INTERVAL = 1000 * 60;
-
 uint32_t syncTime = 0; // time of last sync()
+File logfile;
 
 void error(char *str)
 {
   Serial.print("error: ");
   Serial.println(str);
-  
-  // red LED indicates error
-  digitalWrite(redLEDpin, HIGH);
-  
+
   while(1);
 }
 
 
 int16_t getDistance() {
-//  int16_t distance;  // The last measured distance
-//  bool newData = false; // Whether new data is available from the sensor
-//  uint8_t buffer[4];  // our buffer for storing data
-//  uint8_t idx = 0;  // our idx into the storage buffer
   uint8_t samples = 0;
-  
-  while(samples < 5) {
+
+  while(samples < 3) {
     if (mySerial.available()) {
       uint8_t c = mySerial.read();
-      Serial.println(c, HEX);
-  
+      // Serial.println(c, HEX);
+
       // See if this is a header byte
       if (idx == 0 && c == 0xFF) {
         buffer[idx++] = c;
@@ -91,7 +73,7 @@ int16_t getDistance() {
         }
         idx = 0;
       }
-    }    
+    }
   }
   return distance;
 }
@@ -110,7 +92,7 @@ void setup() {
     abort();
   }
   rtc.start();
-    
+
   // set the data rate for the Serial port, 9600 for the sensor
   mySerial.begin(9600);
   pinMode(CONTROL_PIN, OUTPUT);
@@ -120,16 +102,16 @@ void setup() {
   Serial.print("Initializing SD card...");
   // make sure that the default chip select pin is set to
   // output, even if you don't use it:
-  pinMode(chipSelect, OUTPUT);
-  
+  pinMode(CHIP_SELECT, OUTPUT);
+
   // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
+  if (!SD.begin(CHIP_SELECT)) {
     Serial.println("Card failed, or not present");
     // don't do anything more:
     return;
   }
   Serial.println("card initialized.");
-  
+
   // create a new file
   char filename[] = "LOGGER00.CSV";
   for (uint8_t i = 0; i < 100; i++) {
@@ -137,17 +119,17 @@ void setup() {
     filename[7] = i%10 + '0';
     if (! SD.exists(filename)) {
       // only open a new file if it doesn't exist
-      logfile = SD.open(filename, FILE_WRITE); 
+      logfile = SD.open(filename, FILE_WRITE);
       break;  // leave the loop!
     }
   }
-  
+
   if (! logfile) {
     error("couldnt create file");
   }
-  
+
   Serial.print("Logging to: ");
-  Serial.println(filename);  
+  Serial.println(filename);
 }
 
 
@@ -156,77 +138,20 @@ void loop() { // run over and over
   Serial.print("Will delay for: ");
   Serial.println(delayMsec);
   delay(delayMsec);
-/*
-  if (mySerial.available()) {
-    uint8_t c = mySerial.read();
-    //Serial.println(c, HEX);
 
-    // See if this is a header byte
-    if (idx == 0 && c == 0xFF) {
-      buffer[idx++] = c;
-    }
-    // Two middle bytes can be anything
-    else if ((idx == 1) || (idx == 2)) {
-      buffer[idx++] = c;
-    }
-    else if (idx == 3) {
-      uint8_t sum = 0;
-      sum = buffer[0] + buffer[1] + buffer[2];
-      if (sum == c) {
-        distance = ((uint16_t)buffer[1] << 8) | buffer[2];
-        newData = true;
-      }
-      idx = 0;
-    }
-  }
-  
-  if (newData) {
-    Serial.print("Distance: ");
-    Serial.print(distance);
-    Serial.println(" mm");
-    newData = false;
-  }
-*/
   int16_t lastDist = getDistance();
   DateTime now = rtc.now();
-  
-//  Serial.print(now.year(), DEC);
-//  Serial.print('/');
-//  Serial.print(now.month(), DEC);
-//  Serial.print('/');
-//  Serial.print(now.day(), DEC);
-//  Serial.print(" (");
-//  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-//  Serial.print(") ");
-//  Serial.print(now.hour(), DEC);
-//  Serial.print(':');
-//  Serial.print(now.minute(), DEC);
-//  Serial.print(':');
-//  Serial.print(now.second(), DEC);
-  Serial.print(now.unixtime());  
+
+  Serial.print(now.unixtime());
   Serial.print(",");
   Serial.print(now.timestamp());
   Serial.print(",");
   Serial.print(lastDist);
-  Serial.println(" mm");  
+  Serial.println(" mm");
 
-//  logfile.print(now.year(), DEC);
-//  logfile.print('/');
-//  logfile.print(now.month(), DEC);
-//  logfile.print('/');
-//  logfile.print(now.day(), DEC);
-//  logfile.print(" (");
-//  logfile.print(daysOfTheWeek[now.dayOfTheWeek()]);
-//  logfile.print(") ");
-//  logfile.print(now.hour(), DEC);
-//  logfile.print(':');
-//  logfile.print(now.minute(), DEC);
-//  logfile.print(':');
-//  logfile.print(now.second(), DEC);
-
-  logfile.print(now.unixtime());  
+  logfile.print(now.unixtime());
   logfile.print(",");
-  logfile.print(now.timestamp());  
+  logfile.print(now.timestamp());
   logfile.print(",");
   logfile.println(lastDist);
 
@@ -235,11 +160,9 @@ void loop() { // run over and over
   // which uses a bunch of power and takes time
   if ((millis() - syncTime) < SYNC_INTERVAL) return;
   syncTime = millis();
-  
+
   // blink LED to show we are syncing data to the card & updating FAT!
-  digitalWrite(redLEDpin, HIGH);
   Serial.println("Flushing to disk!");
   logfile.flush();
-  digitalWrite(redLEDpin, LOW);
-  
+
 }
